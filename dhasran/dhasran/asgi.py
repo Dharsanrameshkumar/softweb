738e10721978b22a90cb02be0ccb0f63490e4f5b
Dharsanrameshkumar <rdharsan@322gmail.com>
2023-12-16 21:12:00 +0530

    Success

---
+++ dhasran/dhasran/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dhasran project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dhasran.settings')
+
+application = get_asgi_application()
